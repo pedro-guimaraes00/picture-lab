ecb2b166c19419cd83afe3ccd6736bbedfb856ee
pedro-guimaraes00 <pedrogmendes00@gmail.com>
2020-07-11 16:36:39 -0300

    integra interface Angular com routing Laravel --cria index.blade e rota

---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <base href="/">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Laravel</title>
+    </head>
+    <body>
+        <app-root></app-root>
+        <script type="text/javascript" src="{{ asset("js/runtime.js") }}" defer></script>
+        <script type="text/javascript" src="{{ asset("js/polyfills.js") }}" defer></script>
+        <script type="text/javascript" src="{{ asset("js/styles.js") }}" defer></script>
+        <script type="text/javascript" src="{{ asset("js/vendor.js") }}" defer></script>
+        <script type="text/javascript" src="{{ asset("js/main.js") }}" defer></script>
+    </body>
+</html>
