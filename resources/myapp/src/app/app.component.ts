adbe117039a3e62ec37e9e681508853321d676bb
pedro-guimaraes00 <pedrogmendes00@gmail.com>
2020-07-13 16:52:09 -0300

    Cria uma estrutura 'for' para exibição de posts com dados simulados(array)

--- resources/myapp/src/app/app.component.ts
+++ resources/myapp/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Post } from './post';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myapp';
+  private post: Post[] = [
+    new Post("João", "Meu Post", "Subtítulo", "joao@gmail.com", "Minha mensagem"),
+    new Post("João", "Meu Post", "Subtítulo", "joao@gmail.com", "Minha mensagem"),
+    new Post("João", "Meu Post", "Subtítulo", "joao@gmail.com", "Minha mensagem"),
+    new Post("João", "Meu Post", "Subtítulo", "joao@gmail.com", "Minha mensagem"),
+    new Post("João", "Meu Post", "Subtítulo", "joao@gmail.com", "Minha mensagem"),
+    new Post("João", "Meu Post", "Subtítulo", "joao@gmail.com", "Minha mensagem"),
+    new Post("João", "Meu Post", "Subtítulo", "joao@gmail.com", "Minha mensagem"),
+    new Post("João", "Meu Post", "Subtítulo", "joao@gmail.com", "Minha mensagem"),
+  ];
 }
