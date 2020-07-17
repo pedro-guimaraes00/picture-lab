ec81ac6f877d6f6660d777acb2012d000ee3a129
pedro-guimaraes00 <pedrogmendes00@gmail.com>
2020-07-17 11:44:00 -0300

    cria rotas para api e PostController

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+  
+    public function index()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function like($id)
+    {
+        //
+    }
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/', 'PostController@index');
+Route::post('/', 'PostController@store');
+Route::delete('/{id}', 'PostController@destroy');
+Route::get('/like/{id}', 'PostController@like');
